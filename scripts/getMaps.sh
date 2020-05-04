67927f88a9a72e62cd1a13cecc97b78c0267ef0b
BlivionIaG <kev29lt@gmail.com>
2020-05-04 02:00:54 +0200

    fast dl

--- scripts/getMaps.sh
+++ scripts/getMaps.sh
@@ -9,10 +9,15 @@ url="http://overpass.openstreetmap.fr/api/interpreter"
 timeout=7200
 
 dependencies(){
-    wget `$url?data=[timeout:$timeout];(area[name="Morbihan"][admin_level="6"];)->.a;(node(area.a);<;);out meta;` -O morbihan.osm
-    wget `$url?data=[timeout:$timeout];(area[name="Finistère"][admin_level="6"];)->.a;(node(area.a);<;);out meta;` -O finistere.osm
-    wget `$url?data=[timeout:$timeout];(area[name="Ille-et-Vilaine"][admin_level="6"];)->.a;(node(area.a);<;);out meta;` -O ille-et-vilaine.osm
-    wget `$url?data=[timeout:$timeout];(area[name="Côtes-d'Armor"][admin_level="6"];)->.a;(node(area.a);<;);out meta;` -O cotes-darmor.osm
+    # wget `$url?data=[timeout:$timeout];(area[name="Morbihan"][admin_level="6"];)->.a;(node(area.a);<;);out meta;` -O morbihan.osm
+    # wget `$url?data=[timeout:$timeout];(area[name="Finistère"][admin_level="6"];)->.a;(node(area.a);<;);out meta;` -O finistere.osm
+    # wget `$url?data=[timeout:$timeout];(area[name="Ille-et-Vilaine"][admin_level="6"];)->.a;(node(area.a);<;);out meta;` -O ille-et-vilaine.osm
+    # wget `$url?data=[timeout:$timeout];(area[name="Côtes-d'Armor"][admin_level="6"];)->.a;(node(area.a);<;);out meta;` -O cotes-darmor.osm
+
+    wget https://kevin.le-torch.ovh/finistere.osm
+    wget https://kevin.le-torch.ovh/morbihan.osm
+    wget https://kevin.le-torch.ovh/ille-et-vilaine.osm
+    wget https://kevin.le-torch.ovh/cotes-darmor.osm
 }
 
 configure(){
