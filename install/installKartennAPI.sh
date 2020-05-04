cb30e4262421703a816167f37907da186861889b
BlivionIaG <kev29lt@gmail.com>
2020-05-04 18:30:50 +0200

    lts node

--- install/installKartennAPI.sh
+++ install/installKartennAPI.sh
@@ -34,7 +34,7 @@ dependencies() {
     export NVM_DIR="$HOME/.nvm"
     [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
     [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
-    nvm install node
+    nvm install lts/erbium
     curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
     echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
     sudo apt update
