6b66f4f0c411eac7a56e8c3312190700ecb1595f
BlivionIaG <kev29lt@gmail.com>
2020-05-04 20:20:18 +0200

    apache fix

--- install/installKartennClient.sh
+++ install/installKartennClient.sh
@@ -17,8 +17,8 @@ apacheConf=$'<VirtualHost *:80>
 
     DocumentRoot '$curdir'/www
 
-    ErrorLog \${APACHE_LOG_DIR}/error.log
-    CustomLog \${APACHE_LOG_DIR}/access.log combined
+    ErrorLog ${APACHE_LOG_DIR}/error.log
+    CustomLog ${APACHE_LOG_DIR}/access.log combined
 </VirtualHost>
 # vim: syntax=apache ts=4 sw=4 sts=4 sr noet'
 
